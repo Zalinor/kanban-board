df196d652bc64b567d7758526179aa82759e35bf
Zalinor <zalinordreamer@outlook.com>
2026-07-26 15:23:39 +0300

    Реализован Drag & Drop картчоек между колонками

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,8 @@
 
 let tasks = [
-    {id:1, text: "Пример задачки", status: "todo"}
+    {id:1, text: "", status: "todo"},
+    {id:2, text: "", status: "in-progress"},
+    {id:3, text: "", status: "done"}
 ];
 
 function renderTasks() {
@@ -19,9 +21,16 @@ function renderTasks() {
         const card = document.createElement("div");
         card.className = "card";
         card.textContent = task.text;
+        card.draggable = true; //перетаскивание, без этого свойства перетаскивание невозможно
+        card.dataset.id = task.id; //записываем айди в дом элемент
+        card.addEventListener("dragstart", handleDragStart);
         columns[task.status].appendChild(card);
     });
 }
+
+
+
+// добавляем задачи
 
 const input = document.querySelector("#taskInput");
 const addBtn = document.querySelector("#addTaskBtn");
@@ -43,3 +52,38 @@ addTaskBtn.addEventListener("click", () => {
 });
 
 renderTasks();
+
+//срабатывает когда тянешь карточку
+//Сохраняет id перетаскиваемой карточки чтобы использовать в Drop
+function handleDragStart(event) {
+    event.dataTransfer.setData("text/plain", event.target.dataset.id);
+};
+
+//срабатывает постоянно, пока перетаскиваемый элемент проносят над колонкой
+// preventDefault() Обязателен - без него drop не сработает
+function handleDragOver(event) {
+    event.preventDefault();
+}
+
+document.querySelectorAll(".column").forEach((column) => {
+    column.addEventListener("dragover", handleDragOver);
+    column.addEventListener("drop", handleDrop);
+});
+
+
+//срабатывает когда карточку опустили над колонкой
+//достаем айдишник задачи, меняем статус на id той колонки куда бросили и ререндерим доску
+function handleDrop(event) {
+    event.preventDefault();
+    const taskId = Number(event.dataTransfer.getData("text/plain"));
+    const newStatus = event.currentTarget.id;
+
+    tasks = tasks.map((task) => {
+        if (task.id === taskId) {
+            return {...task, status: newStatus};
+        }
+        return task;
+    });
+
+    renderTasks();
+};
